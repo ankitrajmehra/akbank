6959b433f1aa9957c9b8d724b1f018272d092f84
Ankit Kumar <ankitkumarbkm2021@gmail.com>
2026-05-25 23:07:51 +0530

    home page added

--- akbank/urls.py
+++ akbank/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("AKBANK LIVE 🚀")
+    return HttpResponse("AKBANK WEBSITE LIVE 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('bank/', include('bank.urls')),
 ]
